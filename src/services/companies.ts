8a1addd1ec79d7949b2c26b85a84431854c89123
Ricardo Barbosa <rb.ricardo01@gmail.com>
2020-10-12 18:50:55 -0300

    refactor: fix promises on company services

--- src/services/companies.ts
+++ src/services/companies.ts
@@ -1,33 +1,46 @@
 import axios from 'axios'
 
-interface CompanyServices {
-  url: string
+interface Company {
+  business_name: string
+  city: string
+  location_1: {
+    latitude: string
+    longitude: string
+  }
+  location_account: string
+  location_description: string
+  location_start_date: string
+  naics: string
+  primary_naics_description: string
+  street_address: string
+  zip_code: string
+}
+
+interface CompanyWithMostLocations {
+  name: string
+  qty: number
 }
 
 class CompanyServices {
-  constructor() {
-    this.url = 'http://localhost:3333/companies' //localhost
-  }
+  private url = 'http://localhost:3333/companies'
 
-  async getOldestCompany() {
+  async getOldestCompany(): Promise<Company> {
     const url = `${this.url}/oldest`
     const { data } = await axios.get(url)
     return data
   }
 
-  async getCompanyWithMostLocations() {
+  async getCompanyWithMostLocations(): Promise<CompanyWithMostLocations> {
     const url = `${this.url}/mostLocations`
     const { data } = await axios.get(url)
     return data
   }
 
-  async getCompanies(page) {
+  async getCompanies(page: number): Promise<Company> {
     const url = `${this.url}?page=${page}`
     const { data } = await axios.get(url)
     return data
   }
 }
 
-
-
-export default new CompanyServices
+export default new CompanyServices()
